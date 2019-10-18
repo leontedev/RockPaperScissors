ea5c6d48d7e4f163f229e43ddc372a6cb5e2e960
Mihail87 <leontemihai@gmail.com>
2019-10-18 08:15:23 +0300

    Project setup

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Mihai Leonte on 10/18/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
